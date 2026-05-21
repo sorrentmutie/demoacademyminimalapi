5f94d7d8f886fde1256703655f2c9f6d65e906ae
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2026-05-21 13:46:29 +0200

    Work in progress

--- Academy.API.Minimal/Academy.API.Minimal/Program.cs
+++ Academy.API.Minimal/Academy.API.Minimal/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi();*+
 
 var app = builder.Build();
 
